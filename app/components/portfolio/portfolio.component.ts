7a7f92260737bc4cb72fab558ca459d7e62fd6c6
inham130 <inham130@gmail.com>
2017-05-19 14:00:37 +0400

    add chart component

--- app/components/portfolio/portfolio.component.ts
+++ app/components/portfolio/portfolio.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
     styles: [require('./portfolio.component.scss')]
 })
 export class PortfolioComponent {
+    public dateFrom: Date;
+    public dateTo: Date;
+
+    public onSelectFrom(date: Date) {
+        console.log("onSelectFrom: ", date);
+    }
+
+    public onSelectTo(date: Date) {
+        console.log("onSelectTo: ", date);
+    }
 
 }
